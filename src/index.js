f10c139edd8352de7e9a9745b1e9b3e042d90113
ifmiss <185098535@qq.com>
2018-03-17 19:17:45 +0800

    添加github图标。以及测试d-video 发布npm的效果

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import './a.scss'
 import 'static/font-icon/style.css'
+
+import Dvideo from 'c-video';
+var video = new Dvideo({})
--- webpack.config.js
+++ webpack.config.js
@@ -76,7 +76,7 @@ module.exports = {
 		// 当使用 HTML5 History API 时，任意的 404 响应都可能需要被替代为 index.html。通过传入以下启用：
 		contentBase: "./",
 		// 端口号
-		port: 1995,
+		port: 1996,
 		//当有编译器错误或警告时，在浏览器中显示全屏覆盖。默认禁用。如果您只想显示编译器错误：
 		noInfo: true,
 		// 配置端口号
