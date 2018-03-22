d5f166c7f3bb8578bea560d26e1ba3022196ca37
ifmiss <185098535@qq.com>
2018-03-22 09:46:40 +0800

    尝试测试better-scorll的一些功能方便以后使用

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 import './a.scss'
 import 'static/font-icon/style.css'
+// import './test_scroll.scss'
+// import Bscroll from 'better-scroll'
+// // alert(Bscroll);
+// let test_scroll = document.querySelector('.test_scroll');
+// let scroll = new Bscroll(test_scroll,{
+//     scrollY: true,
+//     click: true,
+//     pullUpLoad: true,
+//     pullDownRefresh: true
+// })
+
+// scroll.on('pullingDown', () => {
+// 	console.log(1)
+// })
+
+// scroll.on('scroll', (pos) => {
+// 	// console.log(pos)
+// })
